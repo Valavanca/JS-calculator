40fdbe81e4ac72275ec8a7e26fecf44789bef9e0
Olexander Husak <valavanca@gmail.com>
2017-07-06 17:28:18 +0300

    base workflow

--- src/js/index.js
+++ src/js/index.js
@@ -54,16 +54,36 @@ BaseCalculator.prototype = { // basic operations
     }
 }
 
+function WorkFlow(context) {
+    var isOperator = false;
+    this.isDoubleOperator = function () {
+        if(isOperator) {
+            return true;
+        } else {
+            isOperator = true;
+            return false;
+        }
+    },
+    this.allowOperator = function() {
+        if(isOperator==true)
+            isOperator = false;
+    }
+}
+
+
 
 function WebInterface() {
     this.cl = new BaseCalculator;
-    this.tempCl = this.cl;
+    this.workFlow = new WorkFlow();
+    this.tempCl = this.cl(0);
     this.tempFunction;
     this.display = {
         input: '',
         output: ''
     };
-
+ 
+    const MAX = 8;
+    
     var that = this;
     var topLine = document.getElementById("input");
     var botLine = document.getElementById("output");
@@ -71,93 +91,94 @@ function WebInterface() {
     //                                                                                                                                                                                      ***********
     //                                                                                                                                                                                                  Methods
     //                                                                                                                                                                                      ***********
+    
+    [].forEach.call(document.getElementsByClassName("number"), (e) => {
+        inputNumbers(e);
+    });
     function inputNumbers (el) {   // input number by clicking
-        el.addEventListener("click", function(e) {
-            that.display.input += this.innerText;
-            topLine.innerText = that.display.input; 
-        })
+        el.addEventListener("click", (e)=>{inputNum(e.target)})
+    }   
+    function inputNum(el) {
+        if(that.display.input.length>MAX) {
+            return undefined
+        }
+        that.workFlow.allowOperator();
+        that.display.input += el.innerText;
+        topLine.innerText = that.display.input; 
     }
 
+                                                                                                                                                                                /******** Operations ************/
+    function templateOperator (symbol, fun) {
+        if( that.workFlow.isDoubleOperator()) {
+            return undefined
+        } else {
+            topLine.innerText = symbol;
+            if(that.tempFunction==null) { // сheck that the answer is already given
+                botLine.innerText = parseFloat(that.display.input) + symbol;
+            } else {
+                botLine.innerText += parseFloat(that.display.input) + symbol;
+            }                                                      
+                        
+            useTempFunction(that);
+            that.tempFunction = fun;
+            that.display.input = '';
+        }         
+    }
+    let operator =  {
+        minus: ()=>{return templateOperator('-', that.tempCl.sub)},
+        plus: ()=>{return templateOperator('+', that.tempCl.add)},
+        mul: ()=>{return templateOperator('\u00B7', that.tempCl.mul)},
+        div: ()=>{return templateOperator('/', that.tempCl.div)},
+     }
     function useTempFunction(context) {
          if(!context.tempFunction) {
             context.tempCl =  context.cl(parseFloat(context.display.input));
             console.log("Init with ", parseFloat(context.display.input));
         } else {
-            context.tempCl = context.tempFunction.call(context.tempCl, parseFloat(context.display.input));
+            context.tempCl = context.tempFunction.call(context.tempCl, parseFloat(context.display.input)||0);
             console.log("call - that.temp ::", context.tempCl);
         }
     }
 
-    [].forEach.call(document.getElementsByClassName("number"), (e) => {
-        inputNumbers(e);
-    });
-    function plus () {             
-        topLine.innerText = '+';                                                             // Plus
-        botLine.innerText += parseFloat(that.display.input) + '+';
-        
-        useTempFunction(that);
-        that.tempFunction = that.tempCl.add;
-        that.display.input = '';
-    }
-    function minus () {
-        topLine.innerText = '-';                                                                             // Minus
-        botLine.innerText += parseFloat(that.display.input) + '-';
-
-        useTempFunction(that);
-        that.tempFunction = that.tempCl.sub;
-        that.display.input = '';
-    }
-    function mul () {
-        topLine.innerText = '\u00B7';                                                                    // Multiplication
-        botLine.innerText += parseFloat(that.display.input) + '\u00B7';
-
-        useTempFunction(that);
-        that.tempFunction = that.tempCl.mul;
-        that.display.input = '';
-    }
-    function div () {
-        topLine.innerText = '/';                                                                                   // Division
-        botLine.innerText += parseFloat(that.display.input) + '/';
-
-        useTempFunction(that);
-        that.tempFunction = that.tempCl.div;
-        that.display.input = '';
-    }
     function equal () { 
+        that.workFlow.allowOperator();
         useTempFunction(that);
         topLine.innerText = '';
-        botLine.innerText += (parseFloat(that.display.input)||'') + '=' + that.tempCl.result;
+        botLine.innerText = getResult(botLine);
 
-        that.tempCl = that.cl;
+        that.tempCl = that.cl(0);
         that.tempFunction = null;
         that.display.input = '';
     }
     function clear (e) { // clear input number
+        that.workFlow.allowOperator();
         that.display.input = '';
         topLine.innerText = 0;
         botLine.innerText  = '';
         that.cl.result = undefined;
     }
-
-
+    function getResult(histoty) {
+        let result = histoty.innerText + (parseFloat(that.display.input)||'') + '=' + parseFloat(that.tempCl.result.toFixed(6));
+        return !!(result.toString().length>MAX*3) ? "Exceeded limit!" : result
+    }
 
     //                                                                                                                                                                                      ***********
     //                                                                                                                                                                                       Get HTML buttons
     //                                                                                                                                                                                      ***********
-    document.getElementById("plus").addEventListener("click",  function (){  
-        plus();
+    document.getElementById("plus").addEventListener("click",  function (){
+        operator.plus();
     });
 
     document.getElementById("minus").addEventListener("click", function() {
-        minus();
+        operator.minus();
     });
 
     document.getElementById("multiplic").addEventListener("click", function() {
-        mul();
+        operator.mul();
     });
 
     document.getElementById("division").addEventListener("click", function() {
-        div();
+        operator.div();
     });
 
     document.getElementById("equal").addEventListener("click", function() {
@@ -171,4 +192,5 @@ function WebInterface() {
 };
 
 
-var interface = new WebInterface();
+
+var calculator = new WebInterface();
